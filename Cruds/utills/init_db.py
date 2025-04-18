8b4ce7721a80ab36feb1cf2be209af681d1195ba
Muhammad Aqeel <12345678+MuhammadAqeel1209@users.noreply.github.com>
2025-04-18 18:27:54 +0500

    update

--- Cruds/utills/init_db.py
+++ Cruds/utills/init_db.py
@@ -1,12 +1,10 @@
 import logging
 from config.database import engine, Base
-# from models.students import Student
 
 # Basic logging setup
 logging.basicConfig(level=logging.INFO)
 
 def create_tables():
-    # Student.__table__.drop(bind=engine)
     logging.info("Creating tables...")
     Base.metadata.create_all(bind=engine)
 
